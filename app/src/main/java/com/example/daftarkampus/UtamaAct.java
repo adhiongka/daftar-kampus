dd6879abf6ca3239f7ba844984e1086b9f4c6f45
adhiongka <adiatnosmk4tkj@gmail.com>
2020-01-29 15:14:58 +0800

    tambah activity

---
+++ app/src/main/java/com/example/daftarkampus/UtamaAct.java
@@ -0,0 +1,14 @@
+package com.example.daftarkampus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UtamaAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utama);
+    }
+}
